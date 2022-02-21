9c58ef7313035876c7db7833fc7700b9a0794019
SegaSG <sega88sg@gmail.com>
2022-02-22 01:35:36 +0300

    feat: add completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArr = bracketsConfig.join('').replace(/,/g, '');
+  let arr = [];
+  for (let bracket of str) {
+    let bracketsIndex = bracketsArr.indexOf(bracket);
+    if (bracketsIndex % 2 === 0) {
+      if (bracket === bracketsArr[bracketsIndex + 1] && arr[arr.length - 1] === bracketsIndex){
+        arr.pop();
+      } else if (bracket === bracketsArr[bracketsIndex + 1] && arr[arr.length - 1] !== bracketsIndex) {
+        arr.push(bracketsIndex)
+      } else {
+        arr.push(bracketsIndex)
+        }
+      } else {
+        if (arr.pop() !== bracketsIndex - 1){
+          return false;
+        }
+    }      
+  }
+  return arr.length === 0
 }
